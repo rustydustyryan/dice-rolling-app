3e97d86acacb83d7e0def4d70d7ca5e468222831
Ryan Andersen <rustydustyryan@gmail.com>
2023-08-01 13:10:56 -0600

    added comments

--- script.js
+++ script.js
@@ -14,16 +14,19 @@ for (let i = 0; i < imgEls.length; i++) {
   imgEls[i].addEventListener('click', () => addImage(i));
 }
 
-// add images to the page
+// add images to page
 function addImage(i) {
+  // clears the page if dice have already been rolled
   if (isRolled) {
     clearFn();
   }
+// add images to contentEl
   const imgEl = document.createElement('img');
   contentEl.appendChild(imgEl);
   imgEl.classList.add('img');
   beginEl.classList.add('hide');
   imgEl.src = imgEls[i].src;
+  // tells us which dice is being added
   imgEl.alt = imgEls[i].alt;
 
   // add event listener so that when the image is clicked it is removed
@@ -44,15 +47,19 @@ function clearFn() {
     img[i].remove();
   }
   beginEl.classList.remove('hide');
+  // clears the text from the roll container
   rollContainer.innerHTML = '';
 }
 
 // select all of the images that have been inserted
 function rollFn() {
+  // displays the total only once when 'roll' is clicked if no images have been inserted
+  if (isRolled) {
+    clearFn();
+  }
   isRolled = true;
   const imgs = contentEl.querySelectorAll('.img');
   let total = 0;
-
   // roll a dice for each image that corresponds to the dice value
   for (let i = 0; i < imgs.length; i++) {
     const img = imgs[i];
@@ -60,12 +67,12 @@ function rollFn() {
     const sides = parseInt(dice.slice(1));
     const roll = Math.floor(Math.random() * sides) + 1;
     total = total + roll;
-
+    // displays the dice and roll value in the roll container
     const rollEl = document.createElement('p');
     rollContainer.appendChild(rollEl);
     rollEl.innerHTML = `${dice}: ${roll}`;
   }
-
+  // displays the total in the roll container
   const totalEl = document.createElement('p');
   rollContainer.appendChild(totalEl);
   totalEl.innerHTML = `Total: ${total}`;
